9ebdb49259a94b257a6b4b766dacb3a73928558d
lukerigby <112788070+lukerigby@users.noreply.github.com>
2023-02-20 16:51:19 +0000

    Delete extensions/CreateWiki directory

--- extensions/CreateWiki/CreateWikiAliases.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$specialPageAliases = [];
-
-$specialPageAliases['en'] = [
-	'CreateWiki' => [ 'CreateWiki' ],
-	'RequestWiki' => [ 'RequestWiki' ],
-	'RequestWikiQueue' => [ 'RequestWikiQueue', 'RequestWikiEdit' ],
-];
--- extensions/CreateWiki/sql/cw_comments.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE /*_*/cw_comments (
-  cw_id INT NOT NULL,
-  cw_comment BLOB NOT NULL,
-  cw_comment_timestamp VARCHAR(32) NOT NULL,
-  cw_comment_user INT(10) NOT NULL
-) /*$wgDBTableOptions*/;
