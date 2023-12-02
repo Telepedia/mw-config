fe8ff2e108412af166dbb9ba60eca24e5c30955f
Original Authority <luke@telepedia.net>
2023-12-02 01:26:39 +0000

    rate page

--- ManageWikiExtensions.php
+++ ManageWikiExtensions.php
@@ -588,6 +588,39 @@ $wgManageWikiExtensions = [
 		],
 		'section' => 'specialpages',
 	],
+	'ratepage' => [
+		'name' => 'RatePage',
+		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:RatePage',
+		'var' => 'wmgUseRatePage',
+		'conflicts' => false,
+		'requires' => [],
+		'install' => [
+			'sql' => [
+				'ratepage_contest' => "$IP/extensions/RatePage/sql/create-table--ratepage-contest.sql",
+				'ratepage_vote' => "$IP/extensions/RatePage/sql/create-table--ratepage-vote.sql"
+			],
+			'permissions' => [
+				'*' => [
+					'permissions' => [
+						'ratepage-vote',
+						'ratepage-contests-view-list',
+					],
+				],
+				'sysop' => [
+					'permissions' => [
+						'ratepage-contests-view-details',
+						'ratepage-contests-edit',
+					],
+				],
+				'bureaucrat' => [
+					'permissions' => [
+						'ratepage-contests-clear',
+					],
+				],
+			],
+		],
+		'section' => 'other',
+	],
 	'css' => [
 		'name' => 'CSS',
 		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:CSS',
