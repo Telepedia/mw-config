7624873fe4ac83792c24d44880031ed75756f2ee
Original Authority <luke@telepedia.net>
2023-12-04 00:53:02 +0000

    temp remove semantic mediawiki

--- LocalWiki.php
+++ LocalWiki.php
@@ -2,12 +2,6 @@
 
 switch ( $wi->dbname ) {
 
-    case 'spicewarswiki':
-        wfLoadExtension( 'SemanticMediaWiki' );
-        enableSemantics( 'spicewars.telepedia.net' );
-        $smwgShowFactbox = SMW_FACTBOX_NONEMPTY;
-        break;
-
     case 'metawiki':
         wfLoadExtension('CheckUser');
         wfLoadExtension( 'WikiDiscover' );
@@ -30,28 +24,19 @@ switch ( $wi->dbname ) {
         wfLoadExtension( 'MigrateUserAccount' );
         $wgMUARemoteWikiContentPath = 'https://witch-hat-atelier.fandom.com/wiki/';
         $wgMUARemoteWikiAPI = 'https://witch-hat-atelier.fandom.com/api.php';
-	      $wgDefaultMobileSkin = "cosmos";
+	    $wgDefaultMobileSkin = "cosmos";
 	      break;
 
     case 'latelierdessorcierswiki':
         wfLoadExtension( 'MigrateUserAccount' );
         $wgMUARemoteWikiContentPath = 'https://latelier-des-sorciers.fandom.com/fr/wiki/';
         $wgMUARemoteWikiAPI = 'https://latelier-des-sorciers.fandom.com/fr/api.php';
-    	  $wgDefaultMobileSkin = "cosmos";
+    	$wgDefaultMobileSkin = "cosmos";
     	  break;
 
     case 'backroomsdewiki':
         wfLoadExtension( 'MigrateUserAccount' );
         $wgMUARemoteWikiContentPath = 'https://backrooms.fandom.com/de/wiki/';
         $wgMUARemoteWikiAPI = 'https://backrooms.fandom.com/de/api.php';
-        break;
-
-    case 'silowiki':
-        wfLoadExtension( 'SemanticMediaWiki' );
-        enableSemantics( 'telepedia.net' );
-        $wgGroupPermissions['sysop']['smw-admin'] = true /* or false */;
-        $wgGroupPermissions['sysop']['smw-pageedit'] = true /* or false */;
-        $wgGroupPermissions['sysop']['smw-patternedit'] = true /* or false */;
-        $wgGroupPermissions['sysop']['smw-schemaedit'] = true /* or false */;
         break;
 }
