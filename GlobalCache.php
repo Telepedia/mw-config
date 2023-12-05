d07ef23bf4fd64b7f5aee81a743c9e9a1de02e84
Original Authority <luke@telepedia.net>
2023-12-05 15:45:51 +0000

    use redis for the jobqueue (T47)

--- GlobalCache.php
+++ GlobalCache.php
@@ -13,3 +13,10 @@ $wgMainCacheType = CACHE_MEMCACHED;
 $wgSessionCacheType = CACHE_MEMCACHED;
 
 $wgMemCachedServers = [ '10.0.0.8:11000' ];
+
+$wgJobTypeConf['default'] = [
+    'class'          => 'JobQueueRedis',
+    'redisServer'    => '10.0.0.7:6379',
+    'redisConfig'    => [],
+    'daemonized'     => true
+ ];
