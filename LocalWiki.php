25aead4c0a5cd1be98082be16d05bcd54c215887
Original Authority <luke@telepedia.net>
2023-12-17 01:29:50 +0000

    add foreign file repo for WHA wiki

--- LocalWiki.php
+++ LocalWiki.php
@@ -31,11 +31,78 @@ switch ( $wi->dbname ) {
         $wgMUARemoteWikiContentPath = 'https://latelier-des-sorciers.fandom.com/fr/wiki/';
         $wgMUARemoteWikiAPI = 'https://latelier-des-sorciers.fandom.com/fr/api.php';
     	$wgDefaultMobileSkin = "cosmos";
-    	  break;
+
+        $wgForeignFileRepos[] = [
+			'class' => ForeignDBViaLBRepo::class,
+			'name' => 'shared-witchhatatelierwiki',
+			'backend' => 'AmazonS3',
+			'url' => 'https://static.telepedia.net/witchhatatelierwiki',
+			'hashLevels' => 2,
+			'thumbScriptUrl' => false,
+			'transformVia404' => true,
+			'hasSharedCache' => true,
+			'descBaseUrl' => 'https://witchhatatelier.telepedia.net/wiki/File:',
+			'scriptDirUrl' => 'https://witchhatatelier.telepedia.net/',
+			'fetchDescription' => true,
+			'descriptionCacheExpiry' => 86400 * 7,
+			'wiki' => 'witchhatatelierwiki',
+			'initialCapital' => true,
+			'zones' => [
+				'public' => [
+					'container' => 'local-public',
+				],
+				'thumb' => [
+					'container' => 'local-thumb',
+				],
+				'temp' => [
+					'container' => 'local-temp',
+				],
+				'deleted' => [
+					'container' => 'local-deleted',
+				],
+			],
+			'abbrvThreshold' => 160
+		];
+
+
+    	break;
 
     case 'backroomsdewiki':
         wfLoadExtension( 'MigrateUserAccount' );
         $wgMUARemoteWikiContentPath = 'https://backrooms.fandom.com/de/wiki/';
         $wgMUARemoteWikiAPI = 'https://backrooms.fandom.com/de/api.php';
+
+        $wgForeignFileRepos[] = [
+            'class' => ForeignDBViaLBRepo::class,
+            'name' => 'shared-witchhatatelierwiki',
+            'backend' => 'AmazonS3',
+            'url' => 'https://static.telepedia.net/witchhatatelierwiki',
+            'hashLevels' => 2,
+            'thumbScriptUrl' => false,
+            'transformVia404' => true,
+            'hasSharedCache' => true,
+            'descBaseUrl' => 'https://witchhatatelier.telepedia.net/wiki/File:',
+            'scriptDirUrl' => 'https://witchhatatelier.telepedia.net/',
+            'fetchDescription' => true,
+            'descriptionCacheExpiry' => 86400 * 7,
+            'wiki' => 'witchhatatelierwiki',
+            'initialCapital' => true,
+            'zones' => [
+                'public' => [
+                    'container' => 'local-public',
+                ],
+                'thumb' => [
+                    'container' => 'local-thumb',
+                ],
+                'temp' => [
+                    'container' => 'local-temp',
+                ],
+                'deleted' => [
+                    'container' => 'local-deleted',
+                ],
+            ],
+            'abbrvThreshold' => 160
+		];
+
         break;
 }
