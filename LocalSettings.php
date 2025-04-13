58a7d68984f5f3c553910825e4a62e745c3f4df8
Original Authority <luke@telepedia.net>
2025-04-13 02:12:50 +0100

    try this

--- LocalSettings.php
+++ LocalSettings.php
@@ -1239,6 +1239,7 @@ if ( $wi->dbname == 'spicewarswiki' || $wi->dbname == 'loginwiki' ) {
 	$globals['wgCookieDomain'] = '.telepedia.net';
 	$globals['wgCookieSameSite'] = null;
 	$globals['wgCookiePath'] = '/';
+	$globals['wgMainStash'] = 'redis';
 }
 
 // phpcs:ignore MediaWiki.Usage.ForbiddenFunctions.extract
