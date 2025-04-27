9885280acf02313c2f702041a17a6ad48329c69c
Original Authority <luke@telepedia.net>
2025-04-27 14:31:31 +0100

    switch readonly mode

--- LocalWiki.php
+++ LocalWiki.php
@@ -45,9 +45,12 @@ switch ( $wi->dbname ) {
 			],
 			'abbrvThreshold' => 160
 		];
-		$wgReadOnly = ( PHP_SAPI === 'cli' ) ? false : 'This wiki is currently being upgraded to a newer software version. Please check back in a couple of hours.';
 		break;
 
+	case 'elatelierdesombrerosdemagowiki':
+		$wgReadOnly = ( PHP_SAPI === 'cli' ) ? false : 'This wiki is currently being upgraded to a newer software version. Please check back in a couple of hours.';
+		break;
+		
 	case 'atelierspiczastychkapeluszywiki':
 		$wgForeignFileRepos[] = [
 			'class' => ForeignDBViaLBRepo::class,
