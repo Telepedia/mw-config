9090bf875abdb3ab28069fcc0d43e566fe8e7ab9
Original Authority <luke@telepedia.net>
2025-05-09 01:10:43 +0100

    re-enable now please

--- LocalSettings.php
+++ LocalSettings.php
@@ -1229,7 +1229,7 @@ $wi->loadExtensions();
 
 require_once __DIR__ . '/ManageWikiNamespaces.php';
 require_once __DIR__ . '/ManageWikiSettings.php';
-// require_once __DIR__ . '/GlobalLogging.php';
+require_once __DIR__ . '/GlobalLogging.php';
 
 $wgUploadDirectory = "{$IP}/images/$wgDBname";
 $wgUploadPath = "{$wgScriptPath}/$wgDBname";
