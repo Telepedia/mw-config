8ca4daf9b3a95b401185beed6ab839fd480d05d0
Original Authority <luke@telepedia.net>
2025-05-16 18:47:09 +0100

    no longer in read only mode

--- LocalWiki.php
+++ LocalWiki.php
@@ -143,8 +143,4 @@ switch ( $wi->dbname ) {
 		$wgVectorNightMode['logged_out'] = true;
 		$wgVectorNightMode['logged_in'] = true;
 		break;
-	
-	case 'newqualitipediawiki':
-		$wgReadOnly = ( PHP_SAPI === 'cli' ) ? false : 'Essential maintenance is in progress. This wiki is in read-only mode. Please check back in around an hour.';
-		break;
 }
