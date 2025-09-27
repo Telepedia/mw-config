0bab3429f5702afbf08be0cd165eb01e0b7c822e
Original Authority <luke@telepedia.net>
2025-09-27 12:27:45 +0100

    FIx path; forgot about the symlink

--- LocalSettings.php
+++ LocalSettings.php
@@ -1306,7 +1306,7 @@ $wgCargoDBname = $wgDBname . 'cargo';
 
 // Temp - to be moved to TelepediaCore ext.
 $wgResourceModules['telepedia.fetch'] = [
-	'scripts' => 'TPRest.js'
+	'scripts' => '/prod/mediawiki/config/TPRest.js'
 ];
 
 // Define last - Extension message files for loading extensions
