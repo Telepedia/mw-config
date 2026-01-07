b25edcdb669be01cb3421275924af7273c4d7010
OriginalAuthority <luke@telepedia.net>
2026-01-07 22:26:04 +0000

    [PLAT-74]: Log slow requests and their context

--- Profiler.php
+++ Profiler.php
@@ -24,5 +24,41 @@ class TelepediaProfiler {
 					json_encode( $data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE ) );
 			} );
 		}
+
+		self::registerSlowLog();
+	}
+
+	/**
+	 * Temporary for PLAT-74; identify which pages are slow to load so that we can profile them.
+	 * StatsD sends the load time to Prometheus, but gives no context on which page, or wiki, it was
+	 *
+	 * We could just put thsi in self::init(), but this allows us to enable and disable it on whim
+	 * @return void
+	 */
+	private static function registerSlowLog(): void {
+		register_shutdown_function( function () {
+			$start = $_SERVER['REQUEST_TIME_FLOAT'] ?? microtime( true );
+			$duration = microtime( true ) - $start;
+
+			// threshold of 300ms for now
+			if ( $duration > 0.3 ) {
+				global $wgDBname;
+				// gather some useful information
+				$url = $_SERVER['REQUEST_URI'] ?? 'Unknown';
+				$clientIP = $_SERVER['REMOTE_ADDR'] ?? 'Unknown';
+
+				$logEntry = sprintf(
+					"[%s] Duration: %.4fs | Wiki: %s | IP: %s | URL: %s\n",
+					date( 'Y-m-d H:i:s' ),
+					$duration,
+					$wgDBname ?? 'unknown',
+					$clientIP,
+					$url
+				);
+
+				// don't care about errors
+				@file_put_contents( '/srv/mediawiki/cache/slow_requests.log', $logEntry, FILE_APPEND );
+			}
+		} );
 	}
 }
