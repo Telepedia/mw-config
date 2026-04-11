f001640428b2da548ca540b13d498eaee19994e8
Original Authority <luke@telepedia.net>
2026-04-11 01:21:05 +0100

    temporarily pause this

--- LocalSettings.php
+++ LocalSettings.php
@@ -6,8 +6,8 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 }
 
 // Start the profiler if requested, before anything else is done
-require_once __DIR__ . '/Profiler.php';
-TelepediaProfiler::setup( 0.01 );
+// require_once __DIR__ . '/Profiler.php';
+// TelepediaProfiler::setup( 0.01 );
 
 // Load some stuff that must be initialise and available as globals before
 // we determine the wiki context
