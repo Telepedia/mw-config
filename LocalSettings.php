3608a21bfe4726ac8ea9fe2bf35caf2b5d44554a
Original Authority <luke@telepedia.net>
2026-07-09 20:27:09 +0100

    maybe dis work

--- LocalSettings.php
+++ LocalSettings.php
@@ -1018,23 +1018,19 @@ $globals['wgCookieSameSite'] = null;
 $globals['wgCookiePath'] = '/';
 
 $globals['wgGroupPermissions']['bot']['skipcaptcha'] = true;
+$globals['wgExtraNamespaces'][2900] = "Map";
+$globals['wgExtraNamespaces'][2901] = "Map_talk";
+$globals['wgNamespacesToBeSearchedDefault'][2900] = true;
+$globals['wgNamespacesWithSubpages'][2900] = false;
+$globals['wgNamespacesWithSubpages'][2901] = false;
+$globals['wgNamespaceContentModels'][2900] = "wikimap";
+$globals['wgContentNamespaces'][] = 2900;
 
 extract( $globals );
 
 $wgHooks['MediaWikiServices'][] = 'LoadWiki::onMediaWikiServices';
 
 require_once __DIR__ . '/GlobalLogging.php';
-
-// Map namespaces for WikiMaps – we're setting them here so they can't be changed just yet
-// must come after namespaces have already been extracted on line 1021
-$wgExtraNamespaces[2900] = "Map";
-$wgExtraNamespaces[2901] = "Map_talk";
-$wgNamespacesToBeSearchedDefault[2900] = true;
-$wgNamespacesWithSubpages[2900] = false;
-$wgNamespacesWithSubpages[2901] = false;
-$wgNamespaceContentModels[2900] = "wikimap";
-$wgContentNamespaces[] = 2900;
-// end map namespaces
 
 $wgUploadDirectory = "{$IP}/images/$wgDBname";
 $wgUploadPath = "{$wgScriptPath}/$wgDBname";
