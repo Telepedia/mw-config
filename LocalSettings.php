3c5f431e6b29c174378a35856ce372c4edca0daa
OriginalAuthority <luke@telepedia.net>
2026-09-05 23:51:46 +0100

    make K8s read-only for testing

--- LocalSettings.php
+++ LocalSettings.php
@@ -1109,7 +1109,7 @@ if (
 }
 
 // Placeholder because I don't want to rewrite this every time
-// $wgReadOnly = ( PHP_SAPI === 'cli' ) ? false : 'This wiki is currently being upgraded to a newer software version. Please check back in a couple of hours.';
+$wgReadOnly = ( PHP_SAPI === 'cli' ) ? false : 'This wiki is a mirror and is currently in read-only mode.';
 
 // Don't need a global here
 unset( $wi );
